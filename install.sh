78fb5d0f603f2496c6b66fab12206e71d86ba8c4
Robin <r.meier@computer-meier.de>
2023-02-07 18:58:50 +0100

    init

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm i3-gaps lxappearance python3-pip unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev bibata-cursor-theme dmenu kitty
+
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+pip3 install pywal
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/compton
+mkdir -p ~/.config/kitty
+cp .config/i3/config ~/.config/i3/config
+cp .config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+cp .config/compton/compton.conf ~/.config/compton/compton.conf
+cp .fehbg ~/.fehbg
+cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+cp -r .wallpaper ~/.wallpaper 
+
+echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
+echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
